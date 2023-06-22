56dd97883cad3f26d0d4ca164c139a3865f79a35
vishalvardhan02 <124067147+vishalvardhan02@users.noreply.github.com>
2023-06-22 23:11:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import uvicorn
 from pydantic import BaseModel
 import gunicorn
 import requests
+import base64
+import github
+
 app = FastAPI()
 
 
@@ -13,6 +16,22 @@ def home():
 
 @app.get('/append_to_file/{text}')
 def append_to_file(text):
-    return "hello"
+    g = github.Github("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")
+
+    repo = g.get_user().get_repo("justfor")
+    file = repo.get_contents("myfile.txt")
+
+    current_content = file.content
+    current_content_decoded = base64.b64decode(current_content).decode()
+    new_content = ''
+    print(current_content_decoded)
+    if current_content_decoded == '\n':
+        new_content = text
+    else:
+        new_content = current_content_decoded + '\n' + '\n' + text 
+    
+    repo.update_file("myfile.txt", "Update myfile.txt", new_content, file.sha)
+
+    return new_content
     
 
